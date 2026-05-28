47cc9c07fc6b69a0e6411f3bf8c5dbab10ddd41f
Will Howat-Comandre <willhowat@gmail.com>
2026-05-28 15:55:41 +0200

    chore: scaffold plugin structure

---
+++ includes/class-cli-command.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * WP-CLI command class for DMG Read More.
+ *
+ * @package dmg-read-more
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Searches published posts containing the dmg/read-more block.
+ *
+ * ## EXAMPLES
+ *
+ *     wp dmg-read-more search
+ *     wp dmg-read-more search --date-after=2024-01-01 --date-before=2024-06-01
+ */
+class DMG_Read_More_CLI {}
